95a0e15f4e0230b2189be2a5847c40309f6e8484
ssvkravi <ssvkravi@hotmail.com>
2023-08-20 17:06:19 +0530

    all files

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "group_name" {}
+# Project variables
+variable "rg_location" {
+  type        = string
+  description = "The location for the deployment"
+}
+variable "rg_name" {
+  type        = string
+  description = "The location for the deployment"
+}
+
